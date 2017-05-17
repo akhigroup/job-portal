5410c6c33c98fa816f60603c70bd7925f6cb53f3
sjsu-sushant <sushantvairagade@outlook.com>
2017-05-17 02:11:39 -0700

    Fixing undefined issue

--- hireme-web/src/js/jobseeker.js
+++ hireme-web/src/js/jobseeker.js
@@ -316,13 +316,65 @@ $(document).ready(function () {
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             data: JSON.stringify({
-                firstName: $("#newTitle").val(),
-                lastName: $("#newCompanyName").val(),
+                jobTitle: $("#newTitle").val(),
+                companyName: $("#newCompanyName").val(),
                 from: $("#newFrom").val(),
-                summary: $("#newTill").val()
+                to: $("#newTill").val()
             }),
             success: function (data) {
+                object = jQuery.parseJSON(JSON.stringify(data));
+                workExperienceList = object.workExperienceList.workExperience;
+                i = 0;
+                for (i = 0; workExperienceList.length > i; i++) {
+                    workExId = workExperienceList[i].workExperienceId;
+                    thisForm = formWorkEx.replace(/workExID/g, workExId);
+                    if ($("#workExFormId" + workExId).length == 0) {
+                        $("#workEx").append(thisForm);
+                    }
+                    $("#title" + workExId).val(workExperienceList[i].jobTitle);
+                    $("#companyName" + workExId).val(workExperienceList[i].companyName);
+                    $("#from" + workExId).val(workExperienceList[i].from);
+                    $("#till" + workExId).val(workExperienceList[i].to);
 
+                    //Event handlers for delete button
+                    $("#workExDelete" + workExId).click(function (e) {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        workExButId = $(this).attr('id');
+                        workExButId = workExButId.replace("workExDelete", "");
+                        $.ajax({
+                            url: url + "/workex/" + workExButId,
+                            type: "DELETE",
+                            success: function () {
+                                $("#workExFormId" + workExButId).remove();
+                            }
+                        });
+                    });
+
+                    //Event handlers for update button
+                    $("#workExUpdate" + workExId).click(function (e) {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        workExButId = $(this).attr('id');
+                        workExButId = workExButId.replace("workExUpdate", "");
+                        $.ajax({
+                            url: url + "/workex/" + workExButId,
+                            type: "POST",
+                            contentType: "application/json; charset=utf-8",
+                            dataType: "json",
+                            data: JSON.stringify({
+                                jobTitle: $("#title" + workExButId).val(),
+                                companyName: $("#companyName" + workExButId).val(),
+                                from: $("#from" + workExButId).val(),
+                                to: $("#till" + workExButId).val()
+                            }),
+                            success: function () {
+                            }
+                        });
+                    });
+                }
+
+                $("#closeAddWorkEx").click();
             }
         });
     });
@@ -487,7 +539,6 @@ $(document).ready(function () {
         //Profile tab
         if (currentTab == '2') {
 
-
             $.get(url, function (data, status) {
                 object = jQuery.parseJSON(JSON.stringify(data));
                 $("#firstName").val(object.jobseeker.firstName);
@@ -497,165 +548,173 @@ $(document).ready(function () {
 
 
                 //Work experience section
-                workExperienceList = object.jobseeker.workExperienceList.workExperience;
-                i = 0;
-                for (i = 0; workExperienceList.length > i; i++) {
-                    workExId = workExperienceList[i].workExperienceId;
-                    thisForm = formWorkEx.replace(/workExID/g, workExId);
-                    if ($("#workExFormId" + workExId).length == 0) {
-                        $("#workEx").append(thisForm);
+                if("undefined" != typeof object.jobseeker.workExperienceList) {
+                    workExperienceList = object.jobseeker.workExperienceList.workExperience;
+                    i = 0;
+                    for (i = 0; workExperienceList.length > i; i++) {
+                        workExId = workExperienceList[i].workExperienceId;
+                        thisForm = formWorkEx.replace(/workExID/g, workExId);
+                        if ($("#workExFormId" + workExId).length == 0) {
+                            $("#workEx").append(thisForm);
+                        }
+                        $("#title" + workExId).val(workExperienceList[i].jobTitle);
+                        $("#companyName" + workExId).val(workExperienceList[i].companyName);
+                        $("#from" + workExId).val(workExperienceList[i].from);
+                        $("#till" + workExId).val(workExperienceList[i].to);
+
+                        //Event handlers for delete button
+                        $("#workExDelete" + workExId).click(function (e) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            workExButId = $(this).attr('id');
+                            workExButId = workExButId.replace("workExDelete", "");
+
+                            $.ajax({
+                                url: url + "/workex/" + workExButId,
+                                type: "DELETE",
+                                success: function () {
+                                    $("#workExFormId" + workExButId).remove();
+                                }
+                            });
+                        });
+
+                        //Event handlers for update button
+                        $("#workExUpdate" + workExId).click(function (e) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            workExButId = $(this).attr('id');
+                            workExButId = workExButId.replace("workExUpdate", "");
+                            $.ajax({
+                                url: url + "/workex/" + workExButId,
+                                type: "POST",
+                                contentType: "application/json; charset=utf-8",
+                                dataType: "json",
+                                data: JSON.stringify({
+                                    jobTitle: $("#title" + workExButId).val(),
+                                    companyName: $("#companyName" + workExButId).val(),
+                                    from: $("#from" + workExButId).val(),
+                                    to: $("#till" + workExButId).val()
+                                }),
+                                success: function () {
+                                }
+                            });
+                        });
                     }
-                    $("#title" + workExId).val(workExperienceList[i].jobTitle);
-                    $("#companyName" + workExId).val(workExperienceList[i].companyName);
-                    $("#from" + workExId).val(workExperienceList[i].from);
-                    $("#till" + workExId).val(workExperienceList[i].to);
-
-                    //Event handlers for delete button
-                    $("#workExDelete" + workExId).click(function (e) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        workExButId = $(this).attr('id');
-                        workExButId = workExButId.replace("workExDelete", "");
-
-                        $.ajax({
-                            url: url + "/workex/" + workExButId,
-                            type: "DELETE",
-                            success: function () {
-                                $("#workExFormId" + workExButId).remove();
-                            }
-                        });
-                    });
-
-                    //Event handlers for update button
-                    $("#workExUpdate" + workExId).click(function (e) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        workExButId = $(this).attr('id');
-                        workExButId = workExButId.replace("workExUpdate", "");
-                        $.ajax({
-                            url: url + "/workex/" + workExButId,
-                            type: "POST",
-                            contentType: "application/json; charset=utf-8",
-                            dataType: "json",
-                            data: JSON.stringify({
-                                jobTitle: $("#title" + workExButId).val(),
-                                companyName: $("#companyName" + workExButId).val(),
-                                from: $("#from" + workExButId).val(),
-                                to: $("#till" + workExButId).val()
-                            }),
-                            success: function () {
-                            }
-                        });
-                    });
                 }
+
 
 
                 //Education Section
-                educationList = object.jobseeker.educationList.education;
-                for (i = 0; educationList.length > i; i++) {
-                    eduId = educationList[i].educationId;
-                    thisForm = formEducation.replace(/EduID/g, eduId);
-                    if ($("#educationFormId" + eduId).length == 0) {
-                        $("#education").append(thisForm);
+                if("undefined" != typeof object.jobseeker.educationList) {
+                    educationList = object.jobseeker.educationList.education;
+                    for (i = 0; educationList.length > i; i++) {
+                        eduId = educationList[i].educationId;
+                        thisForm = formEducation.replace(/EduID/g, eduId);
+                        if ($("#educationFormId" + eduId).length == 0) {
+                            $("#education").append(thisForm);
+                        }
+
+                        $("#school" + eduId).val(educationList[i].schoolName);
+                        $("#degree" + eduId).val(educationList[i].degree);
+                        $("#field" + eduId).val(educationList[i].field);
+                        $("#eduFrom" + eduId).val(educationList[i].from);
+                        $("#eduTill" + eduId).val(educationList[i].to);
+
+                        //Event handlers for delete button
+                        $("#eduDelete" + eduId).click(function (e) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            eduButId = $(this).attr('id');
+                            eduButId = eduButId.replace("eduDelete", "");
+
+                            $.ajax({
+                                url: url + "/education/" + eduButId,
+                                type: "DELETE",
+                                success: function () {
+                                    $("#educationFormId" + eduButId).remove();
+                                }
+                            });
+                        });
+
+                        //Event handlers for update button
+                        $("#eduUpdate" + eduId).click(function (e) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            eduButId = $(this).attr('id');
+                            eduButId = eduButId.replace("eduUpdate", "");
+                            $.ajax({
+                                url: url + "/education/" + eduButId,
+                                type: "POST",
+                                contentType: "application/json; charset=utf-8",
+                                dataType: "json",
+                                data: JSON.stringify({
+                                    schoolName: $("#school" + eduButId).val(),
+                                    degree: $("#degree" + eduButId).val(),
+                                    field: $("field" + eduButId).val(),
+                                    from: $("#eduFrom" + eduButId).val(),
+                                    to: $("#eduTill" + eduButId).val()
+                                }),
+                                success: function () {
+                                }
+                            });
+                        });
+
                     }
-
-                    $("#school" + eduId).val(educationList[i].schoolName);
-                    $("#degree" + eduId).val(educationList[i].degree);
-                    $("#field" + eduId).val(educationList[i].field);
-                    $("#eduFrom" + eduId).val(educationList[i].from);
-                    $("#eduTill" + eduId).val(educationList[i].to);
-
-                    //Event handlers for delete button
-                    $("#eduDelete" + eduId).click(function (e) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        eduButId = $(this).attr('id');
-                        eduButId = eduButId.replace("eduDelete", "");
-
-                        $.ajax({
-                            url: url + "/education/" + eduButId,
-                            type: "DELETE",
-                            success: function () {
-                                $("#educationFormId" + eduButId).remove();
-                            }
-                        });
-                    });
-
-                    //Event handlers for update button
-                    $("#eduUpdate" + eduId).click(function (e) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        eduButId = $(this).attr('id');
-                        eduButId = eduButId.replace("eduUpdate", "");
-                        $.ajax({
-                            url: url + "/education/" + eduButId,
-                            type: "POST",
-                            contentType: "application/json; charset=utf-8",
-                            dataType: "json",
-                            data: JSON.stringify({
-                                schoolName: $("#school" + eduButId).val(),
-                                degree: $("#degree" + eduButId).val(),
-                                field: $("field" + eduButId).val(),
-                                from: $("#eduFrom" + eduButId).val(),
-                                to: $("#eduTill" + eduButId).val()
-                            }),
-                            success: function () {
-                            }
-                        });
-                    });
-
                 }
+
 
                 //Skill Section
-                skillList = object.jobseeker.skillList.skill;
-                for (i = 0; skillList.length > i; i++) {
-                    skillId = skillList[i].skillId;
-                    thisForm = formSkill.replace(/SkillID/g, skillId);
-                    if ($("#skillFormId" + skillId).length == 0) {
-                        $("#skill").append(thisForm);
+                if("undefined" != typeof object.jobseeker.skillList) {
+                    skillList = object.jobseeker.skillList.skill;
+                    for (i = 0; skillList.length > i; i++) {
+                        skillId = skillList[i].skillId;
+                        thisForm = formSkill.replace(/SkillID/g, skillId);
+                        if ($("#skillFormId" + skillId).length == 0) {
+                            $("#skill").append(thisForm);
+                        }
+
+                        $("#skill" + skillId).val(skillList[i].skill);
+                        $("#yearsOfExp" + skillId).val(skillList[i].numberOfYears);
+
+
+                        //Event handlers for delete button
+                        $("#skillDelete" + skillId).click(function (e) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            skillButId = $(this).attr('id');
+                            skillButId = skillButId.replace("skillDelete", "");
+
+                            $.ajax({
+                                url: url + "/skill/" + skillButId,
+                                type: "DELETE",
+                                success: function () {
+                                    $("#skillFormId" + skillButId).remove();
+                                }
+                            });
+                        });
+
+                        //Event handlers for update button
+                        $("#skillUpdate" + skillId).click(function (e) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            skillButId = $(this).attr('id');
+                            skillButId = skillButId.replace("skillUpdate", "");
+                            $.ajax({
+                                url: url + "/skill/" + skillButId,
+                                type: "POST",
+                                contentType: "application/json; charset=utf-8",
+                                dataType: "json",
+                                data: JSON.stringify({
+                                    skill: $("#skill" + skillButId).val(),
+                                    numberOfYears: $("#yearsOfExp" + skillButId).val()
+                                }),
+                                success: function () {
+                                }
+                            });
+                        });
                     }
-
-                    $("#skill" + skillId).val(skillList[i].skill);
-                    $("#yearsOfExp" + skillId).val(skillList[i].numberOfYears);
-
-
-                    //Event handlers for delete button
-                    $("#skillDelete" + skillId).click(function (e) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        skillButId = $(this).attr('id');
-                        skillButId = skillButId.replace("skillDelete", "");
-
-                        $.ajax({
-                            url: url + "/skill/" + skillButId,
-                            type: "DELETE",
-                            success: function () {
-                                $("#skillFormId" + skillButId).remove();
-                            }
-                        });
-                    });
-
-                    //Event handlers for update button
-                    $("#skillUpdate" + skillId).click(function (e) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        skillButId = $(this).attr('id');
-                        skillButId = skillButId.replace("skillUpdate", "");
-                        $.ajax({
-                            url: url + "/skill/" + skillButId,
-                            type: "POST",
-                            contentType: "application/json; charset=utf-8",
-                            dataType: "json",
-                            data: JSON.stringify({
-                                skill: $("#skill" + skillButId).val(),
-                                numberOfYears: $("#yearsOfExp" + skillButId).val()
-                            }),
-                            success: function () {
-                            }
-                        });
-                    });
-
                 }
+
             });
         }
 
